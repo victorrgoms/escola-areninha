ba9df8900c889443c452b55e9a95df7bcc0c1bed
victor <paulovictor.pv05@gmail.com>
2026-02-28 17:17:30 -0300

    Initial commit

---
+++ src/main/java/com/victor/escola_areninha/EscolaAreninhaApplication.java
@@ -0,0 +1,13 @@
+package com.victor.escola_areninha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolaAreninhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscolaAreninhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victor/escola_areninha/EscolaAreninhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.escola_areninha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaAreninhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
